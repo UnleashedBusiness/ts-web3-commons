26193c840c80fd7549dfaf957eead87fec8de662
ShadowsCrypto <ceo@opendapps.cloud>
2023-11-17 09:53:48 +0200

    fixed get absolute url bug with length - 1

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -2,7 +2,7 @@ export default class UrlUtils {
   public static getAbsoluteUrl(baseUrl: string, relativePath: string): string {
     return (
       (baseUrl[baseUrl.length - 1] === "/"
-        ? baseUrl.substring(0, this.length - 1)
+        ? baseUrl.substring(0, baseUrl.length - 1)
         : baseUrl) +
       "/" +
       (relativePath[0] === "/" ? relativePath.substring(1) : relativePath)

5fd70c2239bb86a91feb938b71a147d6ae08553a
ShadowsCrypto <ceo@opendapps.cloud>
2024-02-29 21:40:09 +0200

    fixed export for commonjs modules (nest)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-export * from "./contract/web3-contract.js";
-export * from "./utils/chains.js";
-export * from "./utils/notification.service.js";
-export * from "./utils/transaction-running-helper.service.js";
-export * from "./utils/event-emitter.js";
-export * from "./connection/read-only-web3-connection.service.js";
 export * from "./connection/interface/read-only-web3-connection.js";
 export * from "./connection/interface/wallet-web3-connection.js";
+export * from "./connection/read-only-web3-connection.service.js";
 export * from "./connection/web3-connection.const.js";
-export * from "./contract/error/wallet-connection-required.error.js";
 
-export * from "./contract/utils/contract-toolkit.service.js";
+export * from "./contract/error/wallet-connection-required.error.js";
 export * from "./contract/utils/contract.types.js";
 export * from "./contract/utils/contract-general.config.js";
+export * from "./contract/utils/contract-toolkit.service.js";
+export * from "./contract/web3-contract.js";
 
 export * from './storage/nft-storage.client.js'
 export * from './storage/web3-storage-client.interface.js'
+
+export * from './utils/big-number.utils.js'
+export * from "./utils/chains.js";
+export * from './utils/contract-pipe.utils.js'
+export * from "./utils/event-emitter.js";
+export * from "./utils/notification.service.js";
+export * from "./utils/transaction-running-helper.service.js";
 export * from './utils/url-utils.js'
+
+export * from "./abi/erc20.abi.js";
+export * from "./abi/ierc721.abi.js";
+export * from "./abi/uniswap-factory.abi.js";
+export * from "./abi/uniswap-pair.abi.js";
+export * from "./abi/uniswap-router.abi.js";
+export * from "./abi/weth.abi.js";

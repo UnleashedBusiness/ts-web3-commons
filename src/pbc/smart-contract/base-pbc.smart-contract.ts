d949080584091edce4c016ab9a7bdf99be25b3d1
ShadowsCrypto <ceo@opendapps.cloud>
2025-02-08 16:29:07 +0200

    fix: trees added to the pbc base contract

--- src/pbc/smart-contract/base-pbc.smart-contract.ts
+++ src/pbc/smart-contract/base-pbc.smart-contract.ts
@@ -40,7 +40,7 @@ export abstract class BasePBCSmartContract {
 
   public buildMethodDefinition<R>(call: PBCCallDelegate<R>, loadAvlTreeIndexes: number[] = []): PBCCallDefinition<R> {
     return {
-      buildMultiCall: (callback: (value: R) => Promise<any>) => ToMultiCall(call, callback),
+      buildMultiCall: (callback: (value: R) => Promise<any>) => [ToMultiCall(call, callback), loadAvlTreeIndexes],
       executeCall: (chain: ChainDefinition, contractAddress: string) => {
         return this.pbcClient.call(
             chain,
@@ -83,7 +83,7 @@ export class BasePBCSmartContractInstance<C extends BasePBCSmartContract> {
 
   public buildMethodDefinition<R>(call: PBCCallDelegate<R>, loadAvlTreeIndexes: number[] = []): PBCInstanceCallDefinition<R> {
     return {
-      buildMultiCall: (callback: (value: R) => Promise<any>) => ToMultiCall(call, callback),
+      buildMultiCall: (callback: (value: R) => Promise<any>) => [ToMultiCall(call, callback), loadAvlTreeIndexes],
       executeCall: () => {
         return this.contract.buildMethodDefinition(
             call,

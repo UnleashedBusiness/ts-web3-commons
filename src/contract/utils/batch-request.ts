ec04c7f2f4c8f371318891fd123ff4e6deeed83f
ShadowsCrypto <ceo@opendapps.cloud>
2025-05-30 12:11:32 +0300

    fix: error handling from jsonrpc of eth

--- src/contract/utils/batch-request.ts
+++ src/contract/utils/batch-request.ts
@@ -34,21 +34,29 @@ export class BatchRequest {
         return new Promise(async (resolve, reject) => {
             try {
                 const responses = await this.batch.execute(config);
-
                 let i = 0;
-                for (const response of responses) {
-                    (
-                        response.error !== undefined && response.error.code !== undefined
-                        ? this.callbacks[response.id!][1](`Error received from ETH json rpc with code ${response.error.code}. Message: ${response.error.data}, Data: ${JSON.stringify(response.error.data)}`)
-                        : this.callbacks[response.id!][0](response.result as string)
-                    ).then(() => {
-                        i += 1
 
-                        if (i >= responses.length) {
-                            resolve();
-                        }
-                    }).catch(reject);
-                }
+                const promiseWrapper = <D>(promise: Promise<D>) => promise.then(() => {
+                    i += 1
+
+                    if (i >= responses.length) {
+                        resolve();
+                    }
+                }).catch(reject);
+
+                await Promise.allSettled(responses.map(response => {
+                    if (response.error !== undefined && response.error.code !== undefined) {
+                        return promiseWrapper(this.callbacks[response.id!][1](
+                            `Error received from ETH json rpc with code ${response.error.code}. Message: ${response.error.data}, Data: ${JSON.stringify(response.error.data)}`
+                        ));
+                    } else if (response.result === '0x') {
+                        return promiseWrapper(this.callbacks[response.id!][1](
+                            `Empty response received from ETH json rpc!. Id: ${response.id}`
+                        ));
+                    } else {
+                        return promiseWrapper(this.callbacks[response.id!][0](response.result as string));
+                    }
+                }));
             } catch (e) {
                 reject(e);
             }
